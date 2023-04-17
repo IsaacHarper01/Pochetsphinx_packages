18bb787376ba79a8a5339be764d5e605b9b27d0c
IsaacHarper01 <isaac.anibal.pk@gmail.com>
2023-04-17 17:24:57 -0600

    install file added

---
+++ install_pocket.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#necesary dependencies
+sudo apt-get install cmake
+sudo apt-get install git
+pip install pocketsphinx
+sudo apt update 
+sudo apt install swig
+sudo apt install python3-pyaudio
+
+
+
